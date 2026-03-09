7e3aebc6475825e036ae7a737d4c768e9d29dfdb
Umair Hasan <ioopl.com@gmail.com>
2026-03-09 20:26:31 +0000

    Initial Commit

---
+++ GoTApp/GoTAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoTAppApp.swift
+//  GoTApp
+//
+//  Created by Umair Hasan on 09/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct GoTAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
